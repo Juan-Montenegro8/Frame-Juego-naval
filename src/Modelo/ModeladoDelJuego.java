5198f8090df86c66600bfd8e3d0fbdb7565afcf5
Juan <jj.monte2002@gmail.com>
2024-12-05 21:13:52 -0500

    second commit

--- src/Modelo/ModeladoDelJuego.java
+++ src/Modelo/ModeladoDelJuego.java
@@ -4,7 +4,6 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
 /**
  *
@@ -17,18 +16,18 @@ public class ModeladoDelJuego {
      
     public void ubicar() {
         for (int i = 0; i < 7; i++) {
-             batalla[i][aleatorio.nextInt(7)]='s';
-         }
+            batalla[i][aleatorio.nextInt(7)]='s';
+        }
         
         
     }
-    public void con(JLabel j,JLabel panel, JButton bt) {
+    public void con(JLabel lblcontador,JLabel panel, JButton btnexplocion) {
         if(batalla[0][0]=='s'){
             JOptionPane.showMessageDialog(null, "BARCO HUNDIDO");
-            int ja =Integer.parseInt(j.getText());
-            ja=ja+1;
-            j.setText(""+ja);
-            bt.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Modelo/Explosion.jpeg")));
+            int contador =Integer.parseInt(lblcontador.getText());
+            contador+=1;
+            lblcontador.setText(""+contador);
+            btnexplocion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Modelo/Explosion.jpeg")));
             panel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Modelo/Mar.jpeg")));
         }
     }
